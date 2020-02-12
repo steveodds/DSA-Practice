62028025ff1e0a9666255c365ae117c015e2912b
Stephen Odundo <steveremdi@gmail.com>
2020-02-12 19:38:36 +0300

    Boilerplate Linked List implementation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DSA_Practice
 {
@@ -7,8 +8,14 @@ namespace DSA_Practice
         private static readonly int[] daysOfMonths = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
         static void Main(string[] args)
         {
+            CustomList<int> test = new CustomList<int>();
+            LinkedList<int> testList = new LinkedList<int>();
             
         }
+
+
+
+
 
         static void GetDays()
         {
@@ -20,6 +27,60 @@ namespace DSA_Practice
             catch (System.Exception e)
             {
                 System.Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+
+    public class CustomList<T>
+    {
+        public Node<T> Head { get; set; }
+        public CustomList()
+        {
+            
+        }
+
+        public void Append(T value)
+        {
+            var node = new Node<T>(value);
+            if (Head == null)
+                Head = node;
+            
+        }
+
+        public void Remove(T value)
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+        public bool Contains(T value)
+        {
+
+            return false;
+        }
+
+        public bool IsEmpty()
+        {
+            return false;
+        }
+
+        public class Node<TNode>
+        {
+            public TNode data;
+            public Node<TNode> next;
+
+            public Node(TNode value)
+            {
+                data = value;
+                next = null;
             }
         }
     }
